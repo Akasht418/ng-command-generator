c37e49f74e075cb993cd298565226b04ae0c3aee
Akash <akashdeepthakur16@gmail.com>
2023-07-30 17:03:56 +0530

    Added the code for reseting and setting the form

--- src/app/cmd-gen-parent/main-container/code-gen-area/code-gen-area.component.ts
+++ src/app/cmd-gen-parent/main-container/code-gen-area/code-gen-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -7,11 +8,28 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./code-gen-area.component.scss'],
 })
 export class CodeGenAreaComponent implements OnInit {
+
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [], 
+      code: [],
+    });
+  } 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
   @Input() cmd: any;
 
-  code = new FormControl('');
+  generate(){
+    let name = this.form.get('name')?.value;
+    let fullCommand = this.cmd + " " + name;
+    this.form.get('code')?.patchValue(fullCommand);
+  }
+
+  resetForm(){
+    this.form.get('code')?.reset();
+    this.form.get('name')?.reset();
+  }
 
 }
--- src/app/cmd-gen-parent/main-container/main-container.component.ts
+++ src/app/cmd-gen-parent/main-container/main-container.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CodeGenAreaComponent } from './code-gen-area/code-gen-area.component';
 
 @Component({
   selector: 'app-main-container',
   templateUrl: './main-container.component.html',
-  styleUrls: ['./main-container.component.scss']
+  styleUrls: ['./main-container.component.scss'],
 })
 export class MainContainerComponent {
+
+  @ViewChild('childForm') childForm!: CodeGenAreaComponent;
+
   cmd = '';
-  selectType(type: string){
-    if(type == 'new'){
-      this.cmd = "ng new";
-    }else if(type == 'component'){
-      this.cmd = 'ng generate component'
-    }else if(type == 'pipe'){
-      this.cmd = 'ng generate pipe'
-    }else if(type == 'lib'){
-      this.cmd = 'ng generate library'
+  selectType(type: string) {
+
+    if (type == 'new') {
+      this.cmd = 'ng new';
+    } else if (type == 'component') {
+      this.cmd = 'ng generate component';
+    } else if (type == 'pipe') {
+      this.cmd = 'ng generate pipe';
+    } else if (type == 'lib') {
+      this.cmd = 'ng generate library';
     }
+    this.childForm.resetForm();
   }
+  
 }
